62cfcfb28cded71b7284723e515647049635a45f
lizongping <lizongping@mail.tsinghua.edu.cn>
2022-07-25 17:18:37 +0800

    add choice about auto calculate benchmark segments and each length

--- partition_run_spec2017_m1.sh
+++ partition_run_spec2017_m1.sh
@@ -6,38 +6,55 @@ file_name_append=$2
 #benchmark insts, notion: (this insts) >= (real insts)+(lenseg)
 INSTS=$3
 
+#$4和$5只能2选1
+
 #segment count
 #如果只给了段的个数,那么段长不用提供,脚本会自行计算
 segment_count=$4
-file_prefix=${segment_count}
 
 #=====================
 #the following codes don't need to be modified
 #=====================
-#如果给了段长,那么段的个数$4也需要提供
-if [[ -n ${5} ]]; then
+#如果给了段长,那么段的个数自行计算
+if [[ -n ${5} && ${5} != '-' ]]; then
   lenseg=${5}
+  segment_count=$((INSTS/lenseg))
+  ((mul=segment_count*lenseg))
+  if [[ $mul -lt $INSTS ]];then
+    ((segment_count+=1))
+  fi
 else
   #segment len, div(FLOOR)
-  lenseg=$((INSTS/file_prefix))
+  lenseg=$(( INSTS/segment_count ))
+  ((mul=segment_count*lenseg))
+  if [[ $mul -lt $INSTS ]];then
+    ((lenseg+=1))
+  fi
+
 fi
 
+exec 6<>./running/run.fifo
 #run dir
-dirname=${file_prefix}_seg_${lenseg}_len
+dirname=${segment_count}_seg_${lenseg}_len
 
-echo "$lenseg"
-echo "$dirname"
+#echo "$lenseg"
+#echo "$dirname"
 
 mkdir -p "${workdir}/${dirname}"
 if [[ ! -e "${workdir}"/"${file_name_append}".vgi ]];then
   make itrace -C "${workdir}" NUM_INSNS_TO_COLLECT="${INSTS}"
 fi
+index=0
 for ((i=0;i<INSTS;i=i+lenseg))
 do
+  ((index+=1))
   read -u6
   {
     i_add_lenseg=$((i+lenseg))
-    m1file=${file_prefix}_${i}_${i_add_lenseg}_${file_name_append}
+    if [[ $i_add_lenseg -gt $INSTS ]];then
+      i_add_lenseg=$INSTS
+    fi
+    m1file=${index}_${i}_${i_add_lenseg}_${file_name_append}
     make qtrace -C "${workdir}" JUMP_NUM="${i}" CONVERT_NUM_Vgi_RECS=${lenseg} qtFILE="${m1file}"
     make m1 -C "${workdir}" NUM_INST=${lenseg} CPI_INTERVAL=${lenseg} qtFILE="${m1file}"
     rm -rf "${workdir}"/"${m1file}".qt
