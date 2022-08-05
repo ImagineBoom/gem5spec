01212f8a78e221b3d0f47ed71a5530bf61fcba3b
lizongping <lizongping@mail.tsinghua.edu.cn>
2022-08-05 20:25:02 +0800

    delete judgement about the "result.merge" and the MERGE_FILE_PATH line num if equal

--- runspec_gem5_power/Find_IntervalSize.sh
+++ runspec_gem5_power/Find_IntervalSize.sh
@@ -54,30 +54,22 @@ for ((i=0;i<${#interval_size[@]};i++)) do
           make m1_pipeview pipeFILE=${Simpts}_${Interval_size}_${FILE} pipeARGS="-out_file ${Simpts}_${Interval_size}_${FILE}.txt  -overwrite "
           CPI=`grep 'CMPL: CPI--------------------------------------- .* inst.*' ./${Simpts}_${Interval_size}_${FILE}.results |awk '{print $3}'`
           echo ${Simpts} $Weight $CPI | awk '{print($1" "$2" "$3" "$2*$3)}' >> ./CPI_result/${Interval_size}_Calculate_WeightedCPI.log
-          mv ${Simpts}_${Interval_size}_${FILE}.* M1_result 2>/dev/null
-          mv ${Simpts}_${Interval_size}_${FILE}_trace.* M1_result 2>/dev/null
         else
           :
         fi
+        rm -rf ${Simpts}_${Interval_size}_${FILE}.qt ${Simpts}_${Interval_size}_${FILE}.pipe
+        mv ${Simpts}_${Interval_size}_${FILE}.* M1_result 2>/dev/null
+        mv ${Simpts}_${Interval_size}_${FILE}_trace.* M1_result 2>/dev/null
         echo >&6
       }&
     done
     wait
-    LineNumof_merge=`awk 'END{print NR}' ${MERGE_FILE_PATH}`
-    LineNumof_Calculate_WeightedCPI_log=`awk 'END{print NR}' ./CPI_result/${Interval_size}_Calculate_WeightedCPI.log`
-    if [[ $LineNumof_Calculate_WeightedCPI_log == $LineNumof_merge ]]; then
-      Sum_WeightedCPI=`awk '{s += $4} END {print s}' ./CPI_result/${Interval_size}_Calculate_WeightedCPI.log`
-      sort -n -k 1 ./CPI_result/${Interval_size}_Calculate_WeightedCPI.log | awk 'BEGIN {print "simpts","Weights","CPI","WeightedCPI"} {print $1,$2,$3,$4}' |  column -t > ./CPI_result/${Interval_size}_CPI_result.merge
-      echo ${Interval_size}_${FILE}_SumWeightCPI:"$Sum_WeightedCPI" >> ./CPI_result/${Interval_size}_CPI_result.merge
-      echo ${Interval_size}_${FILE}:"$Sum_WeightedCPI" >> ./CPI_result/${FILE}_CPI_result_${Sum_WeightedCPI}.log
-      break
-    fi
+    Sum_WeightedCPI=`awk '{s += $4} END {print s}' ./CPI_result/${Interval_size}_Calculate_WeightedCPI.log`
+    sort -n -k 1 ./CPI_result/${Interval_size}_Calculate_WeightedCPI.log | awk 'BEGIN {print "simpts","Weights","CPI","WeightedCPI"} {print $1,$2,$3,$4}' |  column -t > ./CPI_result/${Interval_size}_CPI_result.merge
+    echo "${FILE} interval size: ${Interval_size}, sum weighted CPI: $Sum_WeightedCPI" >> ./CPI_result/${Interval_size}_CPI_result.merge
+    echo ${Interval_size}_${FILE}:"$Sum_WeightedCPI" >> ./CPI_result/${FILE}_CPI_result_${Sum_WeightedCPI}.log
   }
 done
 Interval_Number=${#interval_size[@]}
 wait
-#rm -r *.pipe *.qt *.config 2>/dev/null
-LineNumof_CPI_result_log=`awk 'END{print NR}' ./CPI_result/${FILE}_CPI_result_${Sum_WeightedCPI}.log`
-if [[ $Interval_Number == $LineNumof_CPI_result_log ]];then
-  sort -n -r -k 2 -t : ./CPI_result/${FILE}_CPI_result_${Sum_WeightedCPI}.log | awk 'END{printf "The best CPI is " $0}' >> ./CPI_result/${FILE}_CPI_result_${Sum_WeightedCPI}.log
-fi
+sort -n -r -k 2 -t : ./CPI_result/${FILE}_CPI_result_${Sum_WeightedCPI}.log | awk 'END{printf "The best CPI is " $0}' >> ./CPI_result/${FILE}_CPI_result_${Sum_WeightedCPI}.log
