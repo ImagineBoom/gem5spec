58823d7aae697fa6480fca97c3d5f9e01ba5a01f
lizongping <lizongping@mail.tsinghua.edu.cn>
2022-08-05 14:15:21 +0800

    support identify and handle "Created 0 qt records" abnormal situation

--- runspec_gem5_power/Find_IntervalSize.sh
+++ runspec_gem5_power/Find_IntervalSize.sh
@@ -13,7 +13,8 @@ NUM_INSNS_TO_COLLECT=1500m
 Sum_WeightedCPI=0
 
 source ../Set_IntervalSize.sh
-
+source ../../scripts/thread_control.sh
+set_thread_pool
 #echo >&6
 
 #if [[ ! -f ${FILE}.vgi ]];then
@@ -44,14 +45,20 @@ for ((i=0;i<${#interval_size[@]};i++)) do
         Simpts=${Simpts_Array[j]}
         Weight=${Weight_Array[j]}
         make qtrace JUMP_NUM=$[Simpts*Interval_size] CONVERT_NUM_Vgi_RECS=${Interval_size} qtFILE=${Simpts}_${Interval_size}_${FILE}
-        # SCROLL_END=${Interval_size}
-        # 每个片段取前5000条，因为生成5,000,000的文本>12小时
-        make m1 NUM_INST=${Interval_size} CPI_INTERVAL=${Interval_size} qtFILE=${Simpts}_${Interval_size}_${FILE} SCROLL_PIPE=1 SCROLL_BEGIN=1 SCROLL_END=5000
-        make m1_pipeview pipeFILE=${Simpts}_${Interval_size}_${FILE} pipeARGS="-out_file ${Simpts}_${Interval_size}_${FILE}.txt  -overwrite "
-        CPI=`grep 'CMPL: CPI--------------------------------------- .* inst.*' ./${Simpts}_${Interval_size}_${FILE}.results |awk '{print $3}'`
-        echo ${Simpts} $Weight $CPI | awk '{print($1" "$2" "$3" "$2*$3)}' >> ./CPI_result/${Interval_size}_Calculate_WeightedCPI.log
-        mv ${Simpts}_${Interval_size}_${FILE}.* M1_result 2>/dev/null
-        mv ${Simpts}_${Interval_size}_${FILE}_trace.* M1_result 2>/dev/null
+        qt_records=0
+        qt_records=$(grep -oP "Created \d+ qt records" ${Simpts}_${Interval_size}_${FILE}_trace.log |grep -oP "\d+")
+        if [[ $qt_records == 5000000 ]]; then
+          # SCROLL_END=${Interval_size}
+          # 每个片段取前5000条，因为生成5,000,000的文本>12小时
+          make m1 NUM_INST=${Interval_size} CPI_INTERVAL=${Interval_size} qtFILE=${Simpts}_${Interval_size}_${FILE} SCROLL_PIPE=1 SCROLL_BEGIN=1 SCROLL_END=5000
+          make m1_pipeview pipeFILE=${Simpts}_${Interval_size}_${FILE} pipeARGS="-out_file ${Simpts}_${Interval_size}_${FILE}.txt  -overwrite "
+          CPI=`grep 'CMPL: CPI--------------------------------------- .* inst.*' ./${Simpts}_${Interval_size}_${FILE}.results |awk '{print $3}'`
+          echo ${Simpts} $Weight $CPI | awk '{print($1" "$2" "$3" "$2*$3)}' >> ./CPI_result/${Interval_size}_Calculate_WeightedCPI.log
+          mv ${Simpts}_${Interval_size}_${FILE}.* M1_result 2>/dev/null
+          mv ${Simpts}_${Interval_size}_${FILE}_trace.* M1_result 2>/dev/null
+        else
+          :
+        fi
         echo >&6
       }&
     done
