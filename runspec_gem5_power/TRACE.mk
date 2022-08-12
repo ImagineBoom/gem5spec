0ccc85e2042a4f633a5634e7a38e52a2cf85d3f4
Tianhao Yu <yth910@tsinghua.edu.cn>
2022-08-12 16:05:49 +0800

    Update target cpi for spilt data and put in different columns

--- runspec_gem5_power/TRACE.mk
+++ runspec_gem5_power/TRACE.mk
@@ -106,7 +106,9 @@ cpi: $(EXECUTABLE)
 	done;
 	rm -rf ./$(FILE)_CKPS_CPI_sorted.log;\
 	result=`awk '{sum+=$$4}END{print sum}' ./$(FILE)_CKPS_Weighted_CPI.log`;\
-	awk -F, 'NR==1 {print "Checkpoint#","Weights","CPI","WeightedCPI"} {print $$1,$$2,$$3,$$4}' $(FILE)_CKPS_Weighted_CPI.log | column -t >$(FILE)_final_result_$${result}.csv;\
+	sed -i 's/ /,/g' ./$(FILE)_CKPS_Weighted_CPI.log;\
+	sed -i '1i "Checkpoint#","Weights","CPI","WeightedCPI"' ./$(FILE)_CKPS_Weighted_CPI.log;\
+	cp ./$(FILE)_CKPS_Weighted_CPI.log ./$(FILE)_final_result_$${result}.csv;\
 	case_name=$(FILE);\
 	sed -i '$$a The '$$case_name' total weighted cpi is '$$result'' ./$(FILE)_final_result_$${result}.csv;\
 	sed -i '$$G' ./$(FILE)_final_result_$${result}.csv;
